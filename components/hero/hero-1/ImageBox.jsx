41b12c9d1737a401d8b011dbd02830a7a886b0cb
Wajih-09 <wajihwork0902@gmail.com>
2025-03-20 18:06:28 +0500

    home background image

--- components/hero/hero-1/ImageBox.jsx
+++ components/hero/hero-1/ImageBox.jsx
@@ -8,7 +8,7 @@ const ImageBox = () => {
           width={486}
           height={589}
           layout="responsive"
-          src="/images/resource/banner-img-1.png"
+          src="/images/resource/background-1.jpg"
           alt="hero image"
         />
       </figure>
