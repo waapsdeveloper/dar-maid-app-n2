e722500e95a3183ea313d0557385967e35a332e4
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-21 16:10:00 +0500

    small changes

--- components/hero/PopularSearch.jsx
+++ components/hero/PopularSearch.jsx
@@ -2,9 +2,9 @@ const PopularSearch = () => {
   return (
     <div className="popular-searches" data-aos="fade-up" data-aos-delay="1000">
       <span className="title">Popular Searches : </span>
-      <a href="#">Designer</a>, <a href="#">Developer</a>, <a href="#">Web</a>,
-      <a href="#"> IOS</a>, <a href="#">PHP</a>, <a href="#">Senior</a>,
-      <a href="#"> Engineer</a>,
+      <a href="#">Agricultural Skills</a>, <a href="#">Housekeeping</a>, <a href="#">Culinary Arts</a>,
+      <a href="#">Driving & Transportation</a>, <a href="#">Childcare & Parenting</a>, <a href="#">Pet Care & Animal Husbandry</a>,
+      <a href="#">Home Maintenance & Repair</a>, <a href="#">Laundry & Garment Care</a>
     </div>
   );
 };
