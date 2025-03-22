7d829fc112e9cf59a3bb7f274d8e2987ba825d22
Wajih-09 <wajihwork0902@gmail.com>
2025-03-22 16:53:31 +0500

    agency changes

--- components/job-listing-pages/job-list-v1/FilterJobsBox.jsx
+++ components/job-listing-pages/job-list-v1/FilterJobsBox.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import jobs from "../../../data/job-featured";
+import JobSearchForm from "./JobSearchForm";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addCategory,
@@ -51,8 +52,8 @@ const FilterJobsBox = () => {
   const locationFilter = (item) =>
     location !== ""
       ? item?.location
-          ?.toLocaleLowerCase()
-          .includes(location?.toLocaleLowerCase())
+        ?.toLocaleLowerCase()
+        .includes(location?.toLocaleLowerCase())
       : item;
 
   // location filter
@@ -70,26 +71,26 @@ const FilterJobsBox = () => {
   const jobTypeFilter = (item) =>
     jobType?.length !== 0 && item?.jobType !== undefined
       ? jobType?.includes(
-          item?.jobType[0]?.type.toLocaleLowerCase().split(" ").join("-")
-        )
+        item?.jobType[0]?.type.toLocaleLowerCase().split(" ").join("-")
+      )
       : item;
 
   // date-posted filter
   const datePostedFilter = (item) =>
     datePosted !== "all" && datePosted !== ""
       ? item?.created_at
-          ?.toLocaleLowerCase()
-          .split(" ")
-          .join("-")
-          .includes(datePosted)
+        ?.toLocaleLowerCase()
+        .split(" ")
+        .join("-")
+        .includes(datePosted)
       : item;
 
   // experience level filter
   const experienceFilter = (item) =>
     experience?.length !== 0
       ? experience?.includes(
-          item?.experience?.split(" ").join("-").toLocaleLowerCase()
-        )
+        item?.experience?.split(" ").join("-").toLocaleLowerCase()
+      )
       : item;
 
   // salary filter
@@ -156,13 +157,13 @@ const FilterJobsBox = () => {
                 </li>
               ))}
             </ul>
-            
+
             <button className="bookmark-btn">
               <span className="flaticon-bookmark"></span>
             </button>
           </div>
-            
-        <div className=" w-100 mt-3 theme-btn btn-style-three" >Request For Interview</div>
+
+          <div className=" w-100 mt-3 theme-btn btn-style-three" >Request For Interview</div>
         </div>
       </div>
       // End all jobs
@@ -199,103 +200,13 @@ const FilterJobsBox = () => {
 
   return (
     <>
-      <div className="ls-switcher">
-        <div className="show-result">
-          <div className="show-1023">
-            <button
-              type="button"
-              className="theme-btn toggle-filters "
-              data-bs-toggle="offcanvas"
-              data-bs-target="#filter-sidebar"
-            >
-              <span className="icon icon-filter"></span> Filter
-            </button>
-          </div>
-          {/* Collapsible sidebar button */}
-
-          <div className="text">
-            Show <strong>{content?.length}</strong> Agency
-          </div>
+      <section className="page-title">
+        <div className="auto-container">
+          <JobSearchForm />
+          {/* <!-- Job Search Form --> */}
         </div>
-        {/* End show-result */}
+      </section>
 
-        <div className="sort-by">
-          {keyword !== "" ||
-          location !== "" ||
-          destination?.min !== 0 ||
-          destination?.max !== 100 ||
-          category !== "" ||
-          jobType?.length !== 0 ||
-          datePosted !== "" ||
-          experience?.length !== 0 ||
-          salary?.min !== 0 ||
-          salary?.max !== 20000 ||
-          tag !== "" ||
-          sort !== "" ||
-          perPage.start !== 0 ||
-          perPage.end !== 0 ? (
-            <button
-              onClick={clearAll}
-              className="btn btn-danger text-nowrap me-2"
-              style={{ minHeight: "45px", marginBottom: "15px" }}
-            >
-              Clear All
-            </button>
-          ) : undefined}
-
-          <select
-            value={sort}
-            className="chosen-single form-select"
-            onChange={sortHandler}
-          >
-            <option value="">Sort by (default)</option>
-            <option value="asc">Newest</option>
-            <option value="des">Oldest</option>
-          </select>
-          {/* End select */}
-
-          <select
-            onChange={perPageHandler}
-            className="chosen-single form-select ms-3 "
-            value={JSON.stringify(perPage)}
-          >
-            <option
-              value={JSON.stringify({
-                start: 0,
-                end: 0,
-              })}
-            >
-              All
-            </option>
-            <option
-              value={JSON.stringify({
-                start: 0,
-                end: 10,
-              })}
-            >
-              10 per page
-            </option>
-            <option
-              value={JSON.stringify({
-                start: 0,
-                end: 20,
-              })}
-            >
-              20 per page
-            </option>
-            <option
-              value={JSON.stringify({
-                start: 0,
-                end: 30,
-              })}
-            >
-              30 per page
-            </option>
-          </select>
-          {/* End select */}
-        </div>
-      </div>
-      {/* End top filter bar box */}
       <div className="Job-block-parent">{content}</div>
       {/* <!-- List Show More --> */}
       <div className="ls-show-more">
