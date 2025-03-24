6c63a37f65a2178d87ecd01c8fb62cb61776deed
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-25 00:09:54 +0500

    persis user

--- store/store.js
+++ store/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+
 import jobSlice from "../features/job/jobSlice";
 import toggleSlice from "../features/toggle/toggleSlice";
 import filterSlice from "../features/filter/filterSlice";
@@ -8,9 +10,17 @@ import candidateSlice from "../features/candidate/candidateSlice";
 import candidateFilterSlice from "../features/filter/candidateFilterSlice";
 import shopSlice from "../features/shop/shopSlice";
 import authReducer from "../features/auth/authSlice";
+import storage from "redux-persist/lib/storage"; // Uses localStorage
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-    reducer: {
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+
+const rootReducer = combineReducers(
+    {
         auth: authReducer,
         job: jobSlice,
         toggle: toggleSlice,
@@ -20,6 +30,21 @@ export const store = configureStore({
         candidate: candidateSlice,
         candidateFilter: candidateFilterSlice,
         shop: shopSlice,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    }
+);
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: false, // Required for redux-persist
+        }),
 });
+
+
+export const persistor = persistStore(store);
