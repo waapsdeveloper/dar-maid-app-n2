871b75de3633afc348487bc102b2feb668a83d87
Wajih-09 <wajihwork0902@gmail.com>
2025-03-24 16:48:10 +0500

    user data

---
+++ data/users.js
@@ -0,0 +1,56 @@
+module.exports = [
+    {
+        id: "1",
+        name: "Super Admin",
+        email: "superadmin@mail.com",
+        password: "password123",
+        role: "superadmin",
+        status: "active",
+        token: "rnE9kA2UUhr68bx41KvW52nuRhzDLZ2d",
+        picture: "/images/resource/profile-pictures/super-admin.jpg"
+      },
+
+      {
+        id: "2",
+        name: "Employer",
+        email: "employeradmin@mail.com",
+        password: "password123",
+        role: "employer",
+        status: "active",
+        token: "e46seVv1Mi9kayoWacOoFuDwIgy5CqQ8",
+        picture: "/images/resource/profile-pictures/employer.jpg"
+      },
+    
+      {
+        id: "3",
+        name: "Employee",
+        email: "employee@mail.com",
+        password: "password123",
+        role: "employee",
+        status: "active",
+        token: "8SyuPDLtmj1K22L0MonqrQYToS9QWEVl",
+        picture: "/images/maid-gallery/1.jpg"
+      },
+
+      {
+        id: "4",
+        name: "Agency",
+        email: "agency@mail.com",
+        password: "password123",
+        role: "agency",
+        status: "active",
+        token: "wHEZAPvB6rWJVgVvdgiPHHiMwepvMfof",
+        picture: "/images/resource/profile-pictures/agency.jpg"
+      },
+    
+      {
+        id: "5",
+        name: "User",
+        email: "user@mail.com",
+        password: "password123",
+        role: "user",
+        status: "active",
+        token: "qXaP7G8JaGjNi9ITKFKYLYgIKHqNkJiz",
+        picture: "/images/resource/profile-pictures/user.jpg"
+      }
+]
