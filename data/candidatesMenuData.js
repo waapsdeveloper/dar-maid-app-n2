819138bdfb77ba038df7540eb84ceb2b866b398d
ShayanBhatti <shayanbhatti332@gmail.com>
2025-05-01 16:24:39 +0500

    sidebar fully update

--- data/candidatesMenuData.js
+++ data/candidatesMenuData.js
@@ -101,20 +101,6 @@ module.exports = [
     active: "",
   },
   {
-    id: 3,
-    name: "Employer", // Changed from "Post a New Job"
-    icon: "la-user-friends",
-    routePath: "/candidate/employers",
-    active: "",
-  },
-  {
-    id: 4,
-    name: "Agency", // Changed from "Manage Jobs"
-    icon: "la-building",
-    routePath: "/candidate/agencies",
-    active: "",
-  },
-  {
     id: 5,
     name: "Interview", // Changed from "All Applicants"
     icon: "la-comments",
