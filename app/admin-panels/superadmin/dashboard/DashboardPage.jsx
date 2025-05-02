41690c22818c852a503de40e20de1714610b8c2f
Muhammad Razi <muhammadrazi3601@gmail.com>
2025-05-02 18:16:40 +0500

    superadmin dashboard update

--- app/admin-panels/superadmin/dashboard/DashboardPage.jsx
+++ app/admin-panels/superadmin/dashboard/DashboardPage.jsx
@@ -14,38 +14,7 @@ const DashboardPage = () => {
       <div className="row">
         <TopCardBlock />
       </div>
-      <div className="row">
-        <div className="col-xl-7 col-lg-12">
-          {/* <!-- Graph widget --> */}
-          <div className="graph-widget ls-widget">
-            <ProfileChart />
-          </div>
-        </div>
-        <div className="col-xl-5 col-lg-12">
-          {/* <!-- Notification Widget --> */}
-          <div className="notification-widget ls-widget">
-            <div className="widget-title">
-              <h4>Notifications</h4>
-            </div>
-            <div className="widget-content">
-              <Notification />
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-12">
-          {/* <!-- applicants Widget --> */}
-          <div className="applicants-widget ls-widget">
-            <div className="widget-title">
-              <h4>Recent Applicants</h4>
-            </div>
-            <div className="widget-content">
-              <div className="row">
-                <Applicants />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </DsPageOuter>
   );
 };
