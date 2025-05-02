77db6549c621228e562dc612cb6aacf73a3f7ba7
Muhammad Razi <muhammadrazi3601@gmail.com>
2025-05-02 18:20:23 +0500

    employee+employer+agent dashboard update

--- app/admin-panels/employee/dashboard/DashboardPage.jsx
+++ app/admin-panels/employee/dashboard/DashboardPage.jsx
@@ -13,46 +13,7 @@ const DashboardPage = () => {
       </div>
      
 
-      <div className="row">
-        <div className="col-xl-7 col-lg-12">
-          {/* <!-- Graph widget --> */}
-          <div className="graph-widget ls-widget">
-            <ProfileChart />
-          </div>
-          {/* End profile chart */}
-        </div>
-        {/* End .col */}
-
-        <div className="col-xl-5 col-lg-12">
-          {/* <!-- Notification Widget --> */}
-          <div className="notification-widget ls-widget">
-            <div className="widget-title">
-              <h4>Notifications</h4>
-            </div>
-            <div className="widget-content">
-              <Notification />
-            </div>
-          </div>
-        </div>
-        {/* End .col */}
-
-        <div className="col-lg-12">
-          {/* <!-- applicants Widget --> */}
-          <div className="applicants-widget ls-widget">
-            <div className="widget-title">
-              <h4>Jobs Applied Recently</h4>
-            </div>
-            <div className="widget-content">
-              <div className="row">
-                {/* <!-- Candidate block three --> */}
-
-                <JobApplied />
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* End .col */}
-      </div>
+     
       {/* End .row profile and notificatins */}
     </DsPageOuter>
 
